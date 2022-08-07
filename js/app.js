c84ad2b93b5cd8ad411699d3e0f8fae128c04564
CoachemUp <jeffcobb1206@gmail.com>
2022-08-07 03:56:31 -0400

    imputing charts

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//TRAFFIC CHART
+const traffic = document.getElementById('traffic-chart');
+const traffic-chart = new Chart(traffic, {
+    type: 'line',
+    data: {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+        datasets: [()]
+    }
+})
