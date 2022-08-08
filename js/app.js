43471222faffe915be7de2aea89828dab57fa285
CoachemUp <jeffcobb1206@gmail.com>
2022-08-08 00:16:17 -0400

    ver 3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
+/* ALERT BANNER */
+const alertBanner = document.getElementById("alert");
+// create the html for the banner
+alertBanner.innerHTML =
+`<div>
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>`;
+
+
+alertBanner.addEventListener("click", e => {
+    const element = e.target;
+  if(element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+  }
+} );
+
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+    }
+    });
+
 //TRAFFIC CHART
 const traffic = document.getElementById('traffic-chart');
-const traffic-chart = new Chart(traffic, {
+const trafficChart = new Chart(traffic, {
     type: 'line',
     data: {
         labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
